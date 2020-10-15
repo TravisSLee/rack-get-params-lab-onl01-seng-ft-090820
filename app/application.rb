745dc66368510c3d54b6a8c5d4454fe32d111c1b
Travis Lee <travisslee223@gmail.com>
2020-10-15 19:56:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,9 +8,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
+     if @@cart.empty?
+				resp.write "Your cart is empty"
+			else
+				@@cart.each do |item|
+	        resp.write "#{item}\n"
+	      end
+			end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
